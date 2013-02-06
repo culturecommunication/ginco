69e49cf3cc72de991bf16571523b9b3758d12646
Hubert FONGARNAND <hubert.fongarnand@smile.fr>
2013-02-06 16:51:15 +0100

    Finished implementation of getting gitinfo into the about window

--- ginco-admin/src/main/webapp/app/view/AProposWin.js
+++ ginco-admin/src/main/webapp/app/view/AProposWin.js
@@ -3,8 +3,8 @@ Ext.define('HadocApp.view.AProposWin', {
 
     autoShow: true,
     height: 250,
-    width: 400,
-    title: 'A Propos',
+    width: 500,
+    title: '<h2>A Propos</h2>',
     titleAlign: 'center',
     modal: true,
 
@@ -16,6 +16,45 @@ Ext.define('HadocApp.view.AProposWin', {
                 {
                     xtype: 'label',
                     html: '<h1>Hadoc GINCO</h1><br /><p>Gestionnaire de thesaurus du MCC.</p>'
+                },
+                {
+                	xtype: 'form',
+                	title: 'Informations de version',
+                	defaults: {
+                		labelWidth: 150
+                    },
+                	listeners : {
+                		afterrender : {
+                			fn : function (theForm) {
+                				theForm.load({
+                					url : 'services/ui/infoservice/getGitInfo',
+                					method : 'GET'
+                				});
+                			}
+                		}
+                	},
+                	items : [
+                	         {
+                	        	 xtype:'displayfield',
+                	        	 name : 'commitId',
+                	        	 fieldLabel : 'commitId'
+                	         },
+                	         {
+                	        	 xtype:'displayfield',
+                	        	 name : 'gitCommitUserName',
+                	        	 fieldLabel : 'gitCommitUserName'
+                	         },
+                	         {
+                	        	 xtype:'displayfield',
+                	        	 name : 'gitBranch',
+                	        	 fieldLabel : 'Branche'
+                	         },
+                	         {
+                	        	 xtype:'displayfield',
+                	        	 name : 'gitBuildTime',
+                	        	 fieldLabel : 'Date du build'
+                	         }                	         
+                	]
                 }
             ]
         });
