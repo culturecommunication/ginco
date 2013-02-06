16b597da05c6ce3c85d58f387b17c7fe94e28016
François-Régis STOREZ <frsto@smile.fr>
2013-02-06 14:17:16 +0100

    [V1S1 US19] Update javadoc and logging

--- ginco-impl/src/main/java/fr/mcc/ginco/ark/SimpleArkProviderImpl.java
+++ ginco-impl/src/main/java/fr/mcc/ginco/ark/SimpleArkProviderImpl.java
@@ -10,25 +10,29 @@ import fr.mcc.ginco.IIdentifierProvider;
 import fr.mcc.ginco.log.Log;
 import org.slf4j.Logger;
 
+/**
+ * First implementation : independent from the class given in parameter
+ * Always return ARK String randomly generated
+ */
 @Service("simpleArkProviderImpl")
 public class SimpleArkProviderImpl implements IIdentifierProvider {
 	
-	private @Value("${application.ark.nma}") String nma;
-	private @Value("${application.ark.naan}") String naan;
-	private @Log Logger logger;
+	@Value("${application.ark.nma}") private String nma;
+	@Value("${application.ark.naan}") private String naan;
+	@Log  private Logger logger;
 
+	/* (non-Javadoc)
+	 * @see fr.mcc.ginco.IIdentifierProvider#getArkId(java.lang.Class)
+	 */
 	@Override
 	public String getArkId(Class clazz) {
-		//First implementation : independent from the class given in parameter
-		//Always return ARK String randomly generated
 		String arkId=null;
 		try {
 			UUID nq = UUID.randomUUID();
 			arkId = new URL(nma + "/ark:/"+ naan + "/" + nq.toString()).toString();
 	        logger.info("Generating ARK Id  | Value generated : "+ arkId);
 	    } catch (MalformedURLException malformedURLException) {
-	        malformedURLException.printStackTrace();
-	        logger.error("Error creating ARK Id | Value generated : "+ arkId);
+	        logger.error("Error creating ARK Id | Value generated : "+ arkId, malformedURLException);
 	    }
 		return arkId;
 	}	
