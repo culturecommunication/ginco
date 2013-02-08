4aabd6ab8dfbfb352319a77ac919ac3d8c3fec0a
Hubert FONGARNAND <hubert.fongarnand@smile.fr>
2013-02-08 11:19:25 +0100

    First splash screen implementation

--- ginco-admin/src/main/webapp/app.js
+++ ginco-admin/src/main/webapp/app.js
@@ -1,3 +1,19 @@
+// Splashscreen handling
+var splashscreen;
+
+Ext.onReady(function() {
+    // Start the mask on the body and get a reference to the mask
+    splashscreen = Ext.getBody().mask('GINCO Chargement...', 'splashscreen');
+    // Add a new class to this mask as we want it to look different from the default.
+    splashscreen.addCls('splashscreen');
+    // Insert a new div before the loading icon where we can place our logo.
+    Ext.DomHelper.insertFirst(Ext.query('.x-mask-msg')[0], {
+        cls: 'x-splash-icon'
+    });
+
+});
+
+
 Ext.Loader.setConfig({
     enabled: true
 });
@@ -35,7 +51,31 @@ Ext.application({
         'MainTreeController',
         'ThesaurusFormController'
     ],
+    init: function(){
+    	//Ext.getBody().mask('Loading...');
+    },
     launch: function() {
+        // Setup a task to fadeOut the splashscreen
+        var task = new Ext.util.DelayedTask(function() {
+            // Fade out the body mask
+            splashscreen.fadeOut({
+                duration: 1000,
+                remove:true
+            });
+            // Fade out the icon and message
+            splashscreen.next().fadeOut({
+                duration: 1000,
+                remove:true,
+                listeners: {
+                    afteranimate: function() {
+                        // Set the body as unmasked after the animation
+                        Ext.getBody().unmask();
+                    }
+                }
+            });
+        });
+        // Run the fade 500 milliseconds after launch.
+        task.delay(500);
     	Ext.FocusManager.enable();
         var map = new Ext.util.KeyMap(Ext.getBody(), {
             key: Ext.EventObject.ONE,
