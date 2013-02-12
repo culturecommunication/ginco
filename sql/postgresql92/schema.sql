cca0415df93a014345c31f57a7e20d22b3e9fbd4
Daphné Bellemin <dabel@smile.fr>
2013-02-12 17:41:54 +0100

    LogJournal - Initialization of sql script for sprint V1S2

--- sql/postgresql92/schema.sql
+++ sql/postgresql92/schema.sql
@@ -456,27 +456,6 @@ ALTER TABLE ONLY thesaurus
     ADD CONSTRAINT fk_thesaurus_type FOREIGN KEY (type) REFERENCES thesaurus_type(identifier);
 
 
-CREATE TABLE log_journal (
-    identifier integer NOT NULL,
-    action text NOT NULL,
-    author text NOT NULL,
-    date timestamp without time zone,
-    entityid text NOT NULL,
-    entitytype text NOT NULL
-);
-
-ALTER TABLE ONLY log_journal
-    ADD CONSTRAINT pk_log_journal_identifier PRIMARY KEY (identifier);
-    
-CREATE SEQUENCE log_journal_identifier_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-ALTER SEQUENCE log_journal_identifier_seq OWNED BY log_journal.identifier;
-
 --
 -- PostgreSQL database dump complete
 --
