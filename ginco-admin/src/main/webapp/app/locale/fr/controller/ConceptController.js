aebabbd05f2da143c62afe95584e39b899a017ce
François-Régis STOREZ <frsto@smile.fr>
2013-02-21 17:34:56 +0100

    [V1S2 US47] Implementation of update concept/terms + translation for extjs concept creation

---
+++ ginco-admin/src/main/webapp/app/locale/fr/controller/ConceptController.js
@@ -0,0 +1,12 @@
+Ext.define('GincoApp.locale.fr.controller.ConceptController',
+		{
+			xLoading : 'Chargement',
+			xDeleteMsgLabel : 'Êtes-vous sûr de vouloir supprimer ce concept ?',
+			xDeleteMsgTitle : 'Supprimer le concept ?',
+			xSucessLabel : 'Succès !',
+			xSucessSavedMsg : 'Le concept a été enregistré avec succès',
+			xSucessRemovedMsg : 'Le concept a été supprimé avec succès',
+			xProblemLabel : 'Erreur !',
+			xProblemSaveMsg : 'Impossible de sauvegarder le concept !',
+			xProblemDeleteMsg : 'Impossible de supprimer le concept !'
+		});
