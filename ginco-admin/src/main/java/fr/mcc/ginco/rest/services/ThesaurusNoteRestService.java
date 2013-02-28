f7cafaf9549b83909327d9e4e956e02fd92318ff
François-Régis STOREZ <frsto@smile.fr>
2013-02-28 15:25:59 +0100

    [V1S3 US30] REST Service to get term/concept note types

---
+++ ginco-admin/src/main/java/fr/mcc/ginco/rest/services/ThesaurusNoteRestService.java
@@ -0,0 +1,111 @@
+/**
+ * Copyright or © or Copr. Ministère Français chargé de la Culture
+ * et de la Communication (2013)
+ * <p/>
+ * contact.gincoculture_at_gouv.fr
+ * <p/>
+ * This software is a computer program whose purpose is to provide a thesaurus
+ * management solution.
+ * <p/>
+ * This software is governed by the CeCILL license under French law and
+ * abiding by the rules of distribution of free software. You can use,
+ * modify and/ or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ * <p/>
+ * As a counterpart to the access to the source code and rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty and the software's author, the holder of the
+ * economic rights, and the successive licensors have only limited liability.
+ * <p/>
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading, using, modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean that it is complicated to manipulate, and that also
+ * therefore means that it is reserved for developers and experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systemsand/or
+ * data to be ensured and, more generally, to use and operate it in the
+ * same conditions as regards security.
+ * <p/>
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
+ */
+package fr.mcc.ginco.rest.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.cxf.jaxrs.ext.MessageContext;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Service;
+
+import fr.mcc.ginco.beans.NoteType;
+import fr.mcc.ginco.exceptions.BusinessException;
+import fr.mcc.ginco.extjs.view.ExtJsonFormLoadData;
+import fr.mcc.ginco.log.Log;
+import fr.mcc.ginco.services.INoteTypeService;
+
+/**
+ * Thesaurus Note REST service for all operation on notes (both term or concept notes)
+ * 
+ */
+@Service
+@Path("/thesaurusnoteservice")
+@Produces({ MediaType.APPLICATION_JSON })
+public class ThesaurusNoteRestService {
+	
+	@Context 
+	private MessageContext context;
+	
+	@Inject
+	@Named("noteTypeService")
+	private INoteTypeService noteTypeService;	
+	
+	@Log
+	private Logger logger;
+	
+	/**
+	 * Public method used to get the list of all concept note types in the database.
+	 * 
+	 * @return list of NoteType objects, if not found - {@code null}
+	 * @throws BusinessException 
+	 */
+	@GET
+	@Path("/getConceptNoteTypes")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public ExtJsonFormLoadData<List<NoteType>> getConceptNoteTypes() throws BusinessException {
+		List<NoteType> noteTypes = new ArrayList<NoteType>();
+		noteTypes = noteTypeService.getConceptNoteTypeList();
+		ExtJsonFormLoadData<List<NoteType>> types = new ExtJsonFormLoadData<List<NoteType>>(noteTypes);
+		types.setTotal((long) noteTypes.size());
+		return types;
+	}
+	
+	/**
+	 * Public method used to get the list of all concept note types in the database.
+	 * 
+	 * @return list of NoteType objects, if not found - {@code null}
+	 * @throws BusinessException 
+	 */
+	@GET
+	@Path("/getTermNoteTypes")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public ExtJsonFormLoadData<List<NoteType>> getTermNoteTypes() throws BusinessException {
+		List<NoteType> noteTypes = new ArrayList<NoteType>();
+		noteTypes = noteTypeService.getTermNoteTypeList();
+		ExtJsonFormLoadData<List<NoteType>> types = new ExtJsonFormLoadData<List<NoteType>>(noteTypes);
+		types.setTotal((long) noteTypes.size());
+		return types;
+	}
+	
+}
