a40112e79d44fd250dd48781e623423358ed2e9d
Hubert FONGARNAND <hubert.fongarnand@smile.fr>
2013-02-28 15:33:01 +0100

    Replace checkColumn by a more accessible one...

--- ginco-admin/src/main/webapp/extjs/ux/CheckColumn.js
+++ ginco-admin/src/main/webapp/extjs/ux/CheckColumn.js
@@ -37,81 +37,48 @@ Ext.define('Ext.ux.CheckColumn', {
 
     tdCls: Ext.baseCSSPrefix + 'grid-cell-checkcolumn',
 
-    constructor: function() {
-        this.addEvents(
-            /**
-             * @event beforecheckchange
-             * Fires when before checked state of a row changes.
-             * The change may be vetoed by returning `false` from a listener.
-             * @param {Ext.ux.CheckColumn} this CheckColumn
-             * @param {Number} rowIndex The row index
-             * @param {Boolean} checked True if the box is to be checked
-             */
-            'beforecheckchange',
-            /**
-             * @event checkchange
-             * Fires when the checked state of a row changes
-             * @param {Ext.ux.CheckColumn} this CheckColumn
-             * @param {Number} rowIndex The row index
-             * @param {Boolean} checked True if the box is now checked
-             */
-            'checkchange'
-        );
-        this.callParent(arguments);
-    },
+    constructor: function(cfg) {
+        var me = this;
+        me.tpl = ['<tpl for=".">','placeholder','</tpl>']; // shouldn't
+        // ever be used. Ignore it.
+        var tplChecked = Ext.create('Ext.XTemplate', [
+          '<tpl for=".">',
+          '<div class="x-field x-form-item x-field-default x-form-cb-checked">',
+          '<div class="x-form-item-body x-form-cb-wrap" role="presentation">',
+          '<input class="x-form-field x-form-checkbox" type="button" autocomplete="off" aria-checked="true" aria-invalid="false" role="checkbox" data-errorqtip="">',
+          '</div>',
+          '</div>',
+          '</tpl>'
+        ]);
+        var tplUnchecked = Ext.create('Ext.XTemplate', [
+          '<tpl for=".">',
+          '<div class="x-field x-form-item x-field-default">',
+          '<div class="x-form-item-body x-form-cb-wrap" role="presentation">',
+          '<input class="x-form-field x-form-checkbox" type="button" autocomplete="off" aria-checked="false" aria-invalid="false" role="checkbox" data-errorqtip="">',
+          '</div>',
+          '</div>',
+          '</tpl>'
+        ]);
 
-    /**
-     * @private
-     * Process and refire events routed from the GridView's processEvent method.
-     */
-    processEvent: function(type, view, cell, recordIndex, cellIndex, e, record, row) {
-        var me = this,
-            key = type === 'keydown' && e.getKey(),
-            mousedown = type == 'mousedown';
+        me.callParent(arguments);
+        me.renderer = function(value, p, record) {
+          var data = Ext.apply({}, record.data, record.getAssociatedData());
+          if (data[me.dataIndex]) {
+            return tplChecked.apply(data);
+          } else {
+            return tplUnchecked.apply(data);
+          }
+        };
 
-        if (mousedown || (key == e.ENTER || key == e.SPACE)) {
-            var dataIndex = me.dataIndex,
-                checked = !record.get(dataIndex);
-
-            // Allow apps to hook beforecheckchange
-            if (me.fireEvent('beforecheckchange', me, recordIndex, checked) !== false) {
-                record.set(dataIndex, checked);
-                me.fireEvent('checkchange', me, recordIndex, checked);
-
-                // Mousedown on the now nonexistent cell causes the view to blur, so stop it continuing.
-                if (mousedown) {
-                    e.stopEvent();
-                }
-
-                // Selection will not proceed after this because of the DOM update caused by the record modification
-                // Invoke the SelectionModel unless configured not to do so
-                if (!me.stopSelection) {
-                    view.selModel.selectByPosition({
-                        row: recordIndex,
-                        column: cellIndex
-                    });
-                }
-
-                // Prevent the view from propagating the event to the selection model - we have done that job.
-                return false;
-            } else {
-                // Prevent the view from propagating the event to the selection model if configured to do so.
-                return !me.stopSelection;
-            }
-        } else {
-            return me.callParent(arguments);
+      },
+        processEvent : function(type, view, cell, recordIndex, cellIndex, e){
+          var me = this;
+          if (type == 'click') {
+            var rec = view.getStore().getAt(recordIndex);
+            rec.set(this.dataIndex, (rec.get(this.dataIndex))?false:true);
+          } else if (type == 'mousedown') {
+            return false;
+          }
+          return me.callParent(arguments);
         }
-    },
-
-    // Note: class names are not placed on the prototype bc renderer scope
-    // is not in the header.
-    renderer : function(value){
-        var cssPrefix = Ext.baseCSSPrefix,
-            cls = [cssPrefix + 'grid-checkheader'];
-
-        if (value) {
-            cls.push(cssPrefix + 'grid-checkheader-checked');
-        }
-        return '<div class="' + cls.join(' ') + '">&#160;</div>';
-    }
 });
