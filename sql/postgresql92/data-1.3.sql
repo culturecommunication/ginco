fc87aa424f5182766eb9454fa8b09077925c43fb
François-Régis STOREZ <frsto@smile.fr>
2013-02-28 12:08:42 +0100

    [V1S3 US30] Adding data1.3.sql with values for note types

---
+++ sql/postgresql92/data-1.3.sql
@@ -0,0 +1,15 @@
+INSERT INTO note_type(
+            code, label, isterm, isconcept)
+    VALUES ('scopeNote','Note d''application', false, true);
+
+INSERT INTO note_type(
+            code, label, isterm, isconcept)
+    VALUES ('historyNote','Note historique', true, true);
+
+INSERT INTO note_type(
+            code, label, isterm, isconcept)
+    VALUES ('definition','Définition', true, false);
+
+INSERT INTO note_type(
+            code, label, isterm, isconcept)
+    VALUES ('editorialNote','Note éditoriale', true, false);
