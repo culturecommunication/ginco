209fb6f84679530552442f7ab92b2022c468c5dc
François-Régis STOREZ <frsto@smile.fr>
2013-02-28 11:01:30 +0100

    [V1S3 US30] Update SQL Schema for notes

--- sql/postgresql92/schema-1.3.sql
+++ sql/postgresql92/schema-1.3.sql
@@ -62,3 +62,57 @@ ALTER TABLE top_relationship
   ADD CONSTRAINT fk_root_top_relationship_thesaurus_concept FOREIGN KEY (rootconceptid)
       REFERENCES thesaurus_concept (identifier) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE CASCADE;
+
+CREATE TABLE note_type
+(
+  code text NOT NULL,
+  libelle text NOT NULL,
+  isterm boolean NOT NULL,
+  isconcept boolean NOT NULL,
+  CONSTRAINT pk_note_type PRIMARY KEY (code),
+  CONSTRAINT chk_not_false_values CHECK (NOT (isterm = false AND isconcept = false))
+);
+      
+CREATE TABLE note
+(
+  identifier text NOT NULL,
+  lexicalvalue text NOT NULL,
+  lang text NOT NULL,
+  source text,
+  notetypecode text NOT NULL,
+  conceptid text,
+  termid text,
+  CONSTRAINT pk_note_identifier PRIMARY KEY (identifier),
+  CONSTRAINT fk_note_languages_iso639 FOREIGN KEY (lang)
+      REFERENCES languages_iso639 (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT fk_note_notetype FOREIGN KEY (notetypecode)
+      REFERENCES note_type (code) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT fk_note_thesaurus_concept FOREIGN KEY (conceptid)
+      REFERENCES thesaurus_concept (identifier) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE,
+  CONSTRAINT fk_note_thesaurus_term FOREIGN KEY (termid)
+      REFERENCES thesaurus_term (identifier) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE
+);
+
+CREATE INDEX fki_note_languages_iso639
+  ON note
+  USING btree
+  (lang);
+
+CREATE INDEX fki_note_notetype
+  ON note
+  USING btree
+  (notetypecode);
+
+CREATE INDEX fki_note_thesaurus_concept
+  ON note
+  USING btree
+  (conceptid);
+
+CREATE INDEX fki_note_thesaurus_term
+  ON note
+  USING btree
+  (termid);
