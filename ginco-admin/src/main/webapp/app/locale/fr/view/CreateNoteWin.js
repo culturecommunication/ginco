75eb07181b80091dad437b4eae819cceed18da95
François-Régis STOREZ <frsto@smile.fr>
2013-03-05 14:47:46 +0100

    [V1S3 US35] Translation for editing notes

---
+++ ginco-admin/src/main/webapp/app/locale/fr/view/CreateNoteWin.js
@@ -0,0 +1,10 @@
+Ext.define('GincoApp.locale.fr.view.CreateTermWin',
+    {
+		xLexicalValueLabel : 'Valeur lexicale',
+		xLanguageLabel : 'Langue',
+		xSourceLabel : 'Source',
+		xTypeLabel : 'Type',
+		xCreateNoteWinTitle : 'Nouvelle note',
+		xSave: 'Enregistrer'
+		
+});
