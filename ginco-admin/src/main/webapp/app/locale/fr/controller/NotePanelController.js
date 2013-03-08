f0b2381266c3df3e0f8f2bd75b5f30113a490916
François-Régis STOREZ <frsto@smile.fr>
2013-03-08 10:25:53 +0100

    [#148378] Modify labels and remove unused

--- ginco-admin/src/main/webapp/app/locale/fr/controller/NotePanelController.js
+++ ginco-admin/src/main/webapp/app/locale/fr/controller/NotePanelController.js
@@ -1,12 +1,8 @@
 Ext.define('GincoApp.locale.fr.controller.NotePanelController',
 		{
 			xLoading : 'Chargement',
-			xDeleteMsgLabel : 'Êtes-vous sûr de vouloir supprimer cette note ?',
-			xDeleteMsgTitle : 'Supprimer la note ?',
 			xSucessLabel : 'Succès !',
-			xSucessSavedMsg : 'La note a été enregistrée avec succès',
-			xSucessRemovedMsg : 'La note a été supprimée avec succès',
+			xSucessSavedMsg : 'Les notes ont été enregistrées avec succès',
 			xProblemLabel : 'Erreur !',
-			xProblemSaveMsg : 'Impossible de sauvegarder la note !',
-			xProblemDeleteMsg : 'Impossible de supprimer la note !'
+			xProblemSaveMsg : 'Echec de la sauvegarde !'
 		});
