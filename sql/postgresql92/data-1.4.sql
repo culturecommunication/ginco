b11b8a08844c6d73acc4561bc6ce995de5582479
François-Régis STOREZ <frsto@smile.fr>
2013-03-12 14:45:17 +0100

    [V1S4 US12] SQL schema for concept groups

---
+++ sql/postgresql92/data-1.4.sql
@@ -0,0 +1,4 @@
+--Concept Group Types
+INSERT INTO concept_group_type VALUES ('1','Domaine');
+INSERT INTO concept_group_type VALUES ('2','Thématique');
+INSERT INTO concept_group_type VALUES ('3','Facette');
---
+++ sql/postgresql92/schema-1.4.sql
@@ -0,0 +1,68 @@
+--DROP SEQUENCE concept_group_type_code_seq;
+--DROP SEQUENCE concept_group_label_identifier_seq;
+--DROP TABLE concept_group_label;
+--DROP TABLE concept_group_concepts;
+--DROP TABLE concept_group;
+--DROP TABLE concept_group_type;
+
+CREATE TABLE concept_group_type
+(
+  code text NOT NULL,
+  label text NOT NULL,
+  CONSTRAINT pk_concept_group_type_code PRIMARY KEY (code)
+);
+
+CREATE SEQUENCE concept_group_type_code_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE concept_group
+(
+  identifier text NOT NULL,
+  thesaurusid text NOT NULL,
+  conceptgrouptypecode text  NOT NULL,
+  CONSTRAINT pk_concept_group_identifier PRIMARY KEY (identifier),
+  CONSTRAINT fk_concept_group_thesaurus_identifier FOREIGN KEY (thesaurusid)
+      REFERENCES thesaurus (identifier) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE,
+  CONSTRAINT fk_concept_group_concept_group_type_code FOREIGN KEY (conceptgrouptypecode)
+      REFERENCES concept_group_type (code) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+CREATE TABLE concept_group_concepts
+(
+  conceptgroupid text NOT NULL,
+  conceptid text NOT NULL,
+  CONSTRAINT pk_concept_group_concepts PRIMARY KEY (conceptgroupid, conceptid),
+  CONSTRAINT fk_concept_group_concepts_concept_group_identifier FOREIGN KEY (conceptgroupid)
+      REFERENCES concept_group (identifier) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT fk_concept_group_concepts_thesaurus_concept_identifier FOREIGN KEY (conceptid)
+      REFERENCES thesaurus_concept (identifier) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+CREATE TABLE concept_group_label
+(
+  identifier integer NOT NULL,
+  lexicalvalue text,
+  created timestamp without time zone DEFAULT now() NOT NULL,
+  modified timestamp without time zone DEFAULT now() NOT NULL,
+  lang text NOT NULL,
+  conceptgroupid text  NOT NULL,
+  CONSTRAINT pk_concept_group_label_identifier PRIMARY KEY (identifier),
+  CONSTRAINT fk_concept_group_label_concept_group_identifier FOREIGN KEY (conceptgroupid)
+      REFERENCES concept_group (identifier) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE
+);
+
+CREATE SEQUENCE concept_group_label_identifier_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
