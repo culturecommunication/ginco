6cda5b597bb9a50d43c3ef4f010662c818b017bc
François-Régis STOREZ <frsto@smile.fr>
2013-03-25 11:32:12 +0100

    [V1S5 US06] Adding check constraint for hidden values

--- sql/postgresql92/schema-1.5.sql
+++ sql/postgresql92/schema-1.5.sql
@@ -1,9 +1,8 @@
 -- ALTER TABLE thesaurus_term DROP COLUMN hidden;
-ALTER TABLE thesaurus_term ADD COLUMN hidden boolean;
-ALTER TABLE thesaurus_term ALTER COLUMN hidden SET NOT NULL;
-ALTER TABLE thesaurus_term ALTER COLUMN hidden SET DEFAULT false;
+ALTER TABLE thesaurus_term ADD COLUMN hidden boolean NOT NULL DEFAULT false;
 
--- ALTER TABLE thesaurus_term_aud DROP COLUMN hidden;
-ALTER TABLE thesaurus_term_aud ADD COLUMN hidden boolean;
-ALTER TABLE thesaurus_term_aud ALTER COLUMN hidden SET NOT NULL;
-ALTER TABLE thesaurus_term_aud ALTER COLUMN hidden SET DEFAULT false;
+--ALTER TABLE thesaurus_term_aud DROP COLUMN hidden;
+ALTER TABLE thesaurus_term_aud ADD COLUMN hidden boolean DEFAULT false;
+
+ALTER TABLE thesaurus_term
+  ADD CONSTRAINT chk_hidden_values CHECK (NOT (prefered = true AND hidden = true));
