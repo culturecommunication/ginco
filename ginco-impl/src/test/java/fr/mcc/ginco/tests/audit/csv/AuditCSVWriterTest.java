06a2f97c4492a75a431cdebcc1748b04e039ea95
Daphné Bellemin <dabel@smile.fr>
2013-04-04 18:21:30 +0200

    Unit tests on audit export

--- ginco-impl/src/test/java/fr/mcc/ginco/tests/audit/csv/AuditCSVWriterTest.java
+++ ginco-impl/src/test/java/fr/mcc/ginco/tests/audit/csv/AuditCSVWriterTest.java
@@ -32,21 +32,33 @@
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL license and that you accept its terms.
  */
-package fr.mcc.ginco.tests.audit;
+package fr.mcc.ginco.tests.audit.csv;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.HashSet;
+import java.util.Set;
 
 import junit.framework.Assert;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import fr.mcc.ginco.audit.csv.AuditCSVWriter;
+import fr.mcc.ginco.audit.csv.JournalEventsEnum;
+import fr.mcc.ginco.audit.csv.JournalLine;
+import fr.mcc.ginco.tests.LoggerTestUtil;
+import fr.mcc.ginco.utils.DateUtil;
 
 public class AuditCSVWriterTest {
 
 	private AuditCSVWriter auditCSVWriter = new AuditCSVWriter();
+	
+	@Before
+	public void init() {
+		LoggerTestUtil.initLogger(auditCSVWriter);
+	}
 	
 	@Test
 	public void testWriteHeader() throws IOException {
@@ -60,4 +72,36 @@ public class AuditCSVWriterTest {
 		Assert.assertEquals(true, sw.toString().startsWith("log-journal.headers.event-type,log-journal.headers.date,log-journal.headers.author,log-journal.headers.conceptId,log-journal.headers.termId,log-journal.headers.role,log-journal.headers.status,log-journal.headers.oldlexicalvalue,log-journal.headers.newlexicalvalue,log-journal.headers.oldParent,log-journal.headers.newparent"));
 		
 	}	
+	
+	
+	@Test
+	public void testWriteJournalLine() throws IOException {
+		StringWriter sw = new StringWriter();
+		BufferedWriter out = new BufferedWriter(sw);
+		
+		JournalLine line = new JournalLine();
+		line.setAuthorId("unknown.author");
+		line.setConceptId("http://fakeconcept");
+		line.setEventDate(DateUtil.dateFromString("2013-01-20 20:30:00"));
+		line.setEventType(JournalEventsEnum.THESAURUSCONCEPT_CREATED);
+		Set<String> newGenericTerms = new HashSet<String>();
+		newGenericTerms.add("http://fakeconcept1");
+		newGenericTerms.add("http://fakeconcept2");
+		line.setNewGenericTerm(new HashSet(newGenericTerms));
+		line.setNewLexicalValue("New term lexical Value");
+		Set<String> oldGenericTerms = new HashSet<String>();
+		oldGenericTerms.add("http://fakeconcept1");
+		line.setOldGenericTerm(oldGenericTerms);
+		line.setOldLexicalValue("Old term lexical value");
+		line.setStatus(0);
+		line.setTermId("http://faketerm");
+		line.setTermRole("TP");
+		
+		auditCSVWriter.writeJournalLine(line, out);
+		out.flush();
+		out.close();
+
+		Assert.assertEquals(true, sw.toString().startsWith("log-journal.thesaurus-concept-created-event,2013-01-20 20:30:00,unknown.author,http://fakeconcept,http://faketerm,TP,concept-status[0],Old term lexical value,New term lexical Value,http://fakeconcept1,http://fakeconcept1|http://fakeconcept2"));
+		
+	}
 }
