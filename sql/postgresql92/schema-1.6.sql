830bfb133f431310bb046492419def18c8c4c92a
François-Régis STOREZ <frsto@smile.fr>
2013-04-09 18:40:03 +0200

    [V1S6 US10] Update SQL script

--- sql/postgresql92/schema-1.6.sql
+++ sql/postgresql92/schema-1.6.sql
@@ -1,4 +1,2 @@
 --Add role to hierarchical relationships
-ALTER TABLE hierarchical_relationship ADD COLUMN role integer;
-ALTER TABLE hierarchical_relationship ALTER COLUMN role SET NOT NULL;
-ALTER TABLE hierarchical_relationship ALTER COLUMN role SET DEFAULT 0;
+ALTER TABLE hierarchical_relationship ADD COLUMN role integer NOT NULL DEFAULT 0;
