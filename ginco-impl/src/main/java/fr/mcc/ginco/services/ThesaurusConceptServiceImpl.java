d2cd2e78413725b7282b145c667073acf9238d0a
Sofiia Fastova <sofiia.fastova@smile.fr>
2013-10-14 13:08:37 +0200

    Added a comment for the 201740 (condition of concept deleting)

--- ginco-impl/src/main/java/fr/mcc/ginco/services/ThesaurusConceptServiceImpl.java
+++ ginco-impl/src/main/java/fr/mcc/ginco/services/ThesaurusConceptServiceImpl.java
@@ -444,6 +444,8 @@ public class ThesaurusConceptServiceImpl implements IThesaurusConceptService {
 	@Override
 	public ThesaurusConcept destroyThesaurusConcept(ThesaurusConcept object)
 			throws BusinessException {
+		/*if (object.getStatus() == ConceptStatusEnum.CANDIDATE.getStatus()
+				|| object.getStatus() == ConceptStatusEnum.REJECTED.getStatus()) {*/
 			List<ThesaurusTerm> terms = thesaurusTermDAO
 					.findTermsByConceptId(object.getIdentifier());
 			for (ThesaurusTerm term : terms) {
@@ -472,6 +474,9 @@ public class ThesaurusConceptServiceImpl implements IThesaurusConceptService {
 			}
 
 			return thesaurusConceptDAO.delete(object);
+			/*} else {
+		           throw new BusinessException("It's not possible to delete a concept with a status different from candidate or rejected", "delete-concept");
+			}*/
 	}
 
 	private void updateConceptTerms(ThesaurusConcept concept,
