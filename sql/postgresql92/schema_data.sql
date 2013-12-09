147cc19bc9825ea249be53cb0f1538cb2287747b
ROLLAND <frederic.rolland@culture.gouv.fr>
2013-12-09 21:56:37 +0100

    Update schema_data.sql

--- sql/postgresql92/schema_data.sql
+++ sql/postgresql92/schema_data.sql
@@ -9855,3 +9855,17 @@ CREATE TABLE thesaurus_array_concept_aud (
     conceptId character varying(255) NOT NULL,
     revtype smallint
 );
+
+-- schema-audit-1.7.sql ----------------------------
+
+ALTER TABLE thesaurus_aud DROP COLUMN format;
+
+CREATE TABLE thesaurus_formats_aud (
+    rev integer NOT NULL,
+    thesaurus_identifier text NOT NULL,
+    format_identifier integer NOT NULL,
+    revtype smallint
+);
+
+ALTER TABLE ONLY thesaurus_formats_aud
+    ADD CONSTRAINT thesaurus_formats_aud_pkey PRIMARY KEY (rev, thesaurus_identifier, format_identifier);
