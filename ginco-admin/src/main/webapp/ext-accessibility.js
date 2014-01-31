2d0aa8f9d6a3916cd1bd6a78d2a2e55543a951dd
Fastova Sofiia <sofiia.fastova@smile.fr>
2014-01-31 14:21:15 +0100

    [#18682] Fix alt tag in menu buttons

--- ginco-admin/src/main/webapp/ext-accessibility.js
+++ ginco-admin/src/main/webapp/ext-accessibility.js
@@ -215,7 +215,7 @@ Ext.define('Thesaurus.ext.KeyMenuItem', {
 					+ Ext.baseCSSPrefix
 					+ 'menu-item-link keymenuitem-link" href="{href}" <tpl if="hrefTarget">target="{hrefTarget}"</tpl> hidefocus="true" unselectable="on">',
 			'<img id="{id}-iconEl" src="{icon}" class="' + Ext.baseCSSPrefix
-					+ 'menu-item-icon {iconCls}" />',
+					+ 'menu-item-icon {iconCls}" alt="" />',
 			'<span id="{id}-textEl" class="' + Ext.baseCSSPrefix
 					+ 'menu-item-text keymenuitem-text">{text}</span>',
 			'<span class="'
@@ -223,7 +223,7 @@ Ext.define('Thesaurus.ext.KeyMenuItem', {
 					+ 'menu-item-text keymenuitem-cmdTxt" <tpl if="menu">style="margin-right: 17px;"</tpl> >{cmdTxt}</span>',
 			'<tpl if="menu">',
 			'<img id="{id}-arrowEl" src="{blank}" class="' + Ext.baseCSSPrefix
-					+ 'menu-item-arrow" />', '</tpl>', '</a>', '</tpl>'],
+					+ 'menu-item-arrow"/>', '</tpl>', '</a>', '</tpl>'],
 	beforeRender : function(ct, pos) {
 		// Intercept the call to onRender so we can add the
 		// keyboard shortcut text to the render data which
