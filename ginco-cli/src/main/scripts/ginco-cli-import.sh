5fa3fb8da1990b83bb4e1b851a2f4622ffbe19b5
satis-ssg <michael-adou.ake@soprasteria.com>
2021-02-22 10:25:09 +0100

    changement nom de version 2.0.9.9 => 2.0.9.10

--- ginco-cli/src/main/scripts/ginco-cli-import.sh
+++ ginco-cli/src/main/scripts/ginco-cli-import.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export "JAVA_HOME=/etc/alternatives/jre"
 export "GINCO_CLI_DIR=/home/satis/ginco/ginco-cli"
-export "JAVA_OPTIONS=-Dfile.encoding=UTF-8 -jar ginco-cli-2.0.9.9.jar"
+export "JAVA_OPTIONS=-Dfile.encoding=UTF-8 -jar ginco-cli-2.0.9.10.jar"
 
 cd $GINCO_CLI_DIR
 $JAVA_HOME/bin/java $JAVA_OPTIONS -i $1
